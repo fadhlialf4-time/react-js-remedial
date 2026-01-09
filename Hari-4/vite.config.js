f741465399936db7489125f1d2304de68d1d3f5a
fdliiialffajri <fadhlialf4@gmail.com>
2026-01-09 11:35:00 +0700

    evaluasi hari 4

--- Hari-4/vite.config.js
+++ Hari-4/vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-=======
-import tailwindcss from '@tailwindcss/vite'
-
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: 
-  [react(),
-  tailwindcss(),
-  ]
->>>>>>> 2ee6cef9bf28ebe98a7db42ec6bd6e75398b16cc
 })
